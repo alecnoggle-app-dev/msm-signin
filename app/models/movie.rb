538893bdc9356a412b93892ba3cf88fa0cf4e0d2
alecnoggle <anoggle@chicagobooth.edu>
2022-03-06 01:28:50 +0000

    70%

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -17,4 +17,8 @@ class Movie < ApplicationRecord
   has_many :characters
 
   has_many :cast, :through => :characters, :source => :actor
+
+  has_many :bookmarks
+
+  has_many :bookmarkers, :through => :bookmarks, :source => :user
 end
